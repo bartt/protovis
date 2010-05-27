85ca1344bff0e8d51c1d49d5d00f71b1d632b18f
Jamie Love <drjlove@gmail.com>
2010-05-27 20:47:17 +1200

    Fix bug that was stopping mouse over/out of one mark working.
    A user was having trouble with panning/resizing due to a hack I'd
    added to avoid infinite mouse events triggering constant redraws.
    In the end this code isn't needed as the constant redraws can be
    avoided by a slightly cleverer approach to handling the mouse over
    and out events in the graphs that were having the problem.

--- protovis-d3.2.js
+++ protovis-d3.2.js
@@ -1,4 +1,4 @@
-// a3997902433091ca80154dab974a0834ed06da83
+// 305b306f705dc55ea45e1b7c29909dd874a86a6b
 /**
  * @class The built-in Array class.
  * @name Array
@@ -7543,28 +7543,6 @@ pv.Mark.prototype.context = function(scene, index, f) {
 /** @private Execute the event listener, then re-render. */
 pv.Mark.dispatch = function(type, scene, index) {
   var m = scene.mark, p = scene.parent, l = m.$handlers[type];
-
-  if (pv.renderer() == 'svgweb' && type != 'mousemove') {
-    // In SVGWeb, when nodes are rerendered, the re-render
-    // can cause SVGWeb to tirgger a mouseover event for the
-    // newly renderer node. As many graphs re-render the same
-    // node, changing color etc, this causes an infinite
-    // stream of mouseover events.
-    //
-    // This hack avoids this, by ensuring we don't
-    // retrigger if we are getting the same event as last time
-    // for the same indexed item, unless it's a mousemove.
-    this.lastDispatchLog = this.lastDispatchLog || {};
-    if (this.lastDispatchLog[type] == index)
-      return;
-    this.lastDispatchLog[type] = index;
-
-    // Ensure if the user mouse-outs of item X, then the mouse
-    // in will work, and vice-versa.
-    if (type == 'mouseover') this.lastDispatchLog['mouseout'] = null;
-    if (type == 'mouseout') this.lastDispatchLog['mouseover'] = null;
-  }
-
   if (!l) return p && pv.Mark.dispatch(type, p, scene.parentIndex);
   m.context(scene, index, function() {
       m = l.apply(m, pv.Mark.stack);
